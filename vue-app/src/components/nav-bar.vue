e22c86a6fb775a185c9c10bd4f1f27b0e2419f75
Lukasz Niedzielski <Lukasz_Niedzielski@epam.com>
2021-06-18 14:24:50 +0200

    changed name

--- vue-app/src/components/nav-bar.vue
+++ vue-app/src/components/nav-bar.vue
@@ -44,7 +44,7 @@ export default {
           <template v-for="provider in providers">
             <a
               :key="provider"
-              :href="`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`"
+              :href="`/.auth/login/azureActiveDirectory`"
               >{{ provider }}</a
             >
           </template>
